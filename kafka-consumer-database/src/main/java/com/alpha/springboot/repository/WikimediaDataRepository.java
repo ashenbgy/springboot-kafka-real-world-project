0aeb97a1b1e3888a62ead86008a75846159035de
ashenbgy <ashenbhagye@gmail.com>
2024-04-09 10:04:27 +0530

    Initial

---
+++ kafka-consumer-database/src/main/java/com/alpha/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.alpha.springboot.repository;
+
+import com.alpha.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
